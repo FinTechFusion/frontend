904a758539a7c5630eda41b9b0820ab7f2a27468
mahmoud <mahmoudshalaby363@gmail.com>
2024-07-23 18:33:46 +0300

    update login code

--- src/components/common/forms/loginform.tsx
+++ src/components/common/forms/loginform.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import { useForm, SubmitHandler } from "react-hook-form";
@@ -12,28 +11,45 @@ export default function Loginform() {
    const { register, handleSubmit, formState: { errors } } = useForm<loginType>({
       mode: "onBlur",
       resolver: zodResolver(loginSchema),
-   })
+   });
+
    const submitForm: SubmitHandler<loginType> = (data) => {
       console.log(data);
    };
+
    const preventPaste = (e: React.ClipboardEvent) => {
-      e.preventDefault()
-   }
+      e.preventDefault();
+   };
 
    return (
       <form onSubmit={handleSubmit(submitForm)}>
          <div className="pb-3">
             <div className="grid grid-cols-1 gap-4">
-               <Input label="email" type="email" register={register} name="email" error={errors.email?.message} placeholder="Email" />
-               <Input label="password" type="password" register={register} name="password" error={errors.password?.message} placeholder="Enter strong password" onPaste={preventPaste}
-
+               <Input
+                  label="email"
+                  type="email"
+                  register={register}
+                  name="email"
+                  error={errors.email?.message}
+                  placeholder="Email"
+               />
+               <Input
+                  label="password"
+                  type="password"
+                  register={register}
+                  name="password"
+                  error={errors.password?.message}
+                  placeholder="Enter strong password"
+                  onPaste={preventPaste}
                />
                <div className="login-btn">
                   <MainBtn content="Login" btnWidth="w-full" />
                </div>
-               <p>Don&apos;t have an account? <Link href="/register" className="text-primary-600 underline">Create</Link></p>
+               <p>
+                  Don&apos;t have an account? <Link href="/register" className="text-primary-600 underline">Create</Link>
+               </p>
             </div>
          </div>
       </form>
-   )
+   );
 }
