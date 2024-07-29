d8c4aa4e8a4f235970c56fd8bf59de47bc523c6a
mahmoud <mahmoudshalaby363@gmail.com>
2024-07-30 01:31:15 +0300

    tes authQuard for dashboard

--- src/app/(user)/dashboard/layout.tsx
+++ src/app/(user)/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "@/app/_components/common/dashboad/sidebar/SideBar"
+import AuthGuard from "@/context/AuthGuard"
 import React from "react"
 
 type DashboardLayoutProps = {
@@ -7,13 +8,15 @@ type DashboardLayoutProps = {
 
 export default function userDahsboardLayout({ children }: DashboardLayoutProps) {
    return (
-      <div className="grid grid-cols-12 gap-x-3">
-         <div className="lg:col-span-2 md:col-span-3 col-span-10">
-            <Sidebar />
+      <AuthGuard>
+         <div className="grid grid-cols-12 gap-x-3">
+            <div className="lg:col-span-2 md:col-span-3 col-span-10">
+               <Sidebar />
+            </div>
+            <div className="lg:col-span-10 md:col-span-9 col-span-1">
+               {children}
+            </div>
          </div>
-         <div className="lg:col-span-10 md:col-span-9 col-span-1">
-            {children}
-         </div>
-      </div>
+      </AuthGuard>
    )
 }
