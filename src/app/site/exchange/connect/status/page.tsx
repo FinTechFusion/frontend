f73b07a437a65b3b59386ed3f1e5fcd2a5e2466b
mahmoud <mahmoudshalaby363@gmail.com>
2024-08-23 02:19:37 +0300

    handle success and error page at connect binance

--- src/app/site/exchange/connect/status/page.tsx
+++ src/app/site/exchange/connect/status/page.tsx
@@ -5,6 +5,11 @@ import useFetch from "@/hooks/useFetch";
 import { API_BASE_URL } from "@/utils/api";
 import { useSearchParams } from "next/navigation";
 import Loading from "@/app/_components/common/loading/Loading";
+import Textbox from "@/app/_components/common/Text/Textbox";
+import { MainBtn } from "@/app/_components/common/Buttons/MainBtn";
+import Link from "next/link";
+import { MdError } from "react-icons/md";
+import { FaCheck } from "react-icons/fa6";
 
 function BinanceConnectionStatus() {
    const searchParams = useSearchParams();
@@ -12,10 +17,24 @@ function BinanceConnectionStatus() {
 
    const { data } = useFetch(`${API_BASE_URL}/users/me/binance/link/callback?code=${code}&code_verifier=${code_verifier}&state=${state}&error=${error}`);
 
+
    return (
       <div className="container mx-auto">
-         <h2>Binance Connection</h2>
-         {data?.success ? "Account Connected Successfully" : "Failed to Connect"}
+         <div className="flex flex-col  justify-start items-center text-center min-h-screen pt-16">
+            {data?.success ? <>
+            <FaCheck className="p-3 bg-primary-600 text-secondary text-6xl rounded-full my-5"/>
+               <Textbox title="Binance Account Connected" description="Your Binance account has been successfully connected. You can now access your trading data and manage your portfolio." descriptionClass="md:w-1/2 w-full mx-auto text-lg" />
+               <Link href='/dashboard'>
+                  <MainBtn content="Go to Dashboard" btnProps="w-fit" />
+               </Link>
+            </> : <>
+               <MdError className="p-3 bg-red-600 text-secondary text-6xl rounded-full my-5 " />
+               <Textbox title="Binance Account Connection Failed" description="We're sorry, but there was an error connecting your Binance account. Please try again later or contact support if the issue persists." descriptionClass="md:w-1/2 w-full mx-auto text-lg" />
+               <Link href='/site/exchange/connect'>
+                  <MainBtn content="try again" btnProps="w-fit" />
+               </Link>
+            </>
+            }</div>   
       </div>
    );
 }
