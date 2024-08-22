7523e5a0b7569aed4edd3737a49dd8172f214a96
mahmoud <mahmoudshalaby363@gmail.com>
2024-08-22 04:27:23 +0300

    use suspense at binance status page

--- src/app/site/exchange/connect/status/page.tsx
+++ src/app/site/exchange/connect/status/page.tsx
@@ -1,19 +1,29 @@
 "use client"
-import useFetch from "@/hooks/useFetch"
+
+import { Suspense } from "react";
+import useFetch from "@/hooks/useFetch";
 import { API_BASE_URL } from "@/utils/api";
 import { useSearchParams } from "next/navigation";
+import Loading from "@/app/_components/common/loading/Loading";
 
-export default function Page() {
+function BinanceConnectionStatus() {
    const searchParams = useSearchParams();
    const [code, code_verifier, state, error] = ['code', 'code_verifier', 'state', 'error'].map(param => searchParams.get(param));
-   // const route = useRouter();
 
-   const { data, loading } = useFetch(`${API_BASE_URL}/users/me/binance/link/callback?code=${code}&code_verifier=${code_verifier}&state=${state}&error=${error}`);
+   const { data } = useFetch(`${API_BASE_URL}/users/me/binance/link/callback?code=${code}&code_verifier=${code_verifier}&state=${state}&error=${error}`);
 
    return (
       <div className="container mx-auto">
          <h2>Binance Connection</h2>
-         {data?.success ? "Account Connected Successfully" : "Faliled at connected"}
+         {data?.success ? "Account Connected Successfully" : "Failed to Connect"}
       </div>
-   )
+   );
+}
+
+export default function Page() {
+   return (
+      <Suspense fallback={<Loading />}>
+         <BinanceConnectionStatus />
+      </Suspense>
+   );
 }
