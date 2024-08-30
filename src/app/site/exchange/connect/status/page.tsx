0de6e204ee87df8bc242970bfc8978118e716f6f
mahmoud <mahmoudshalaby363@gmail.com>
2024-08-30 03:51:16 +0300

    add loading spinner while get status binance connection data

--- src/app/site/exchange/connect/status/page.tsx
+++ src/app/site/exchange/connect/status/page.tsx
@@ -16,7 +16,6 @@ function BinanceConnectionStatus() {
    const accessToken = getTokenFromStorage("access_token");
    const searchParams = useSearchParams();
    const code = searchParams.get('code');
- 
 
    const { data } = useFetch(`${API_BASE_URL}/users/me/binance/link/callback?code=${code}`,
       {
@@ -28,23 +27,26 @@ function BinanceConnectionStatus() {
    );
 
    return (
-      <div className="container mx-auto">
-         <div className="flex flex-col justify-start items-center text-center min-h-screen pt-16">
-            {data?.is_binance ? <>
-            <FaCheck className="p-3 bg-primary-600 text-secondary text-6xl rounded-full my-5"/>
-               <Textbox title="Binance Account Connected" description="Your Binance account has been successfully connected. You can now access your trading data and manage your portfolio." descriptionClass="md:w-1/2 w-full mx-auto text-lg" />
-               <Link href='/dashboard'>
-                  <MainBtn content="Go to Dashboard" btnProps="w-fit" />
-               </Link>
-            </> : <>
-               <MdError className="p-3 bg-red-600 text-secondary text-6xl rounded-full my-5 " />
-               <Textbox titleClass="hover:text-red-700" title="Binance Account Connection Failed" description="We're sorry, but there was an error connecting your Binance account. Please try again later or contact support if the issue persists." descriptionClass="md:w-1/2 w-full mx-auto text-lg" />
-               <Link href='/site/exchange/connect'>
+      <>
+         {data ? <div className="container mx-auto">
+            <div className="flex flex-col justify-start items-center text-center min-h-screen pt-16">
+               {data?.is_binance ? <>
+                  <FaCheck className="p-3 bg-primary-600 text-secondary text-6xl rounded-full my-5" />
+                  <Textbox title="Binance Account Connected" description="Your Binance account has been successfully connected. You can now access your trading data and manage your portfolio." descriptionClass="md:w-1/2 w-full mx-auto text-lg" />
+                  <Link href='/dashboard'>
+                     <MainBtn content="Go to Dashboard" btnProps="w-fit" />
+                  </Link>
+               </> : <>
+                  <MdError className="p-3 bg-red-600 text-secondary text-6xl rounded-full my-5 " />
+                  <Textbox titleClass="hover:text-red-700" title="Binance Account Connection Failed" description="We're sorry, but there was an error connecting your Binance account. Please try again later or contact support if the issue persists." descriptionClass="md:w-1/2 w-full mx-auto text-lg" />
+                  <Link href='/site/exchange/connect'>
                      <MainBtn content="try again" btnProps="w-fit bg-red-600 hover:bg-red-700" />
-               </Link>
-            </>
-            }</div>   
-      </div>
+                  </Link>
+               </>
+               }</div>
+         </div> : <Loading />}
+      </>
+
    );
 }
 
