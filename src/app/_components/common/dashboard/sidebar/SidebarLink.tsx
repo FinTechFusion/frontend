0e26d9ec4f6e42eac0477c2216b477af4aaeebe8
mahmoud <mahmoudshalaby363@gmail.com>
2024-09-28 16:08:03 +0300

    enable user to switch between type of account while is connected with biannce

--- src/app/_components/common/dashboard/sidebar/SidebarLink.tsx
+++ src/app/_components/common/dashboard/sidebar/SidebarLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 const SidebarLink = ({ icon, content, path, weight,toggleVisibility }: SidebarLinkProps) => {
    return (
-      <Link  className={`hover:bg-gray-300 p-3 rounded mb-2 mt-2 text-xl text-primary-600 flex justify-start items-center ${weight}`} href={`${path}`}>
+      <Link onClick={toggleVisibility} className={`hover:bg-gray-300 p-3 rounded mb-2 mt-2 text-xl text-primary-600 flex justify-start items-center ${weight}`} href={`${path}`}>
          <span className="px-2 text-xl">{icon}</span>
          <p >{content}</p>
       </Link>)
