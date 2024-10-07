932c682e8dc6826fe5a06807f68e3693bd8bd909
mahmoud <mahmoudshalaby363@gmail.com>
2024-10-07 19:51:26 +0300

    set default lang is ar at navigation

--- src/i18n/navigation.ts
+++ src/i18n/navigation.ts
@@ -2,5 +2,5 @@ import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 
 export const { Link, useRouter, usePathname } = createSharedPathnamesNavigation({
    locales: ['en', 'ar'],
-   defaultLocale: 'en'
+   defaultLocale: 'ar'
 });
