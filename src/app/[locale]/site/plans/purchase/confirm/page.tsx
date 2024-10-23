59758168e9c3ed6c6502fe0d6d0c11ad162b360a
mahmoud <mahmoudshalaby363@gmail.com>
2024-10-23 14:22:15 +0300

    edit middleware config

--- src/app/[locale]/site/plans/purchase/confirm/page.tsx
+++ src/app/[locale]/site/plans/purchase/confirm/page.tsx
@@ -1,19 +1,18 @@
 "use client"
 import FailedPayment from "@/app/_components/paymentstatus/FailedPayment";
 import SuccessPayment from "@/app/_components/paymentstatus/SuccessPayment";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import useFetch from '@/hooks/useFetch';
 import { API_BASE_URL } from '@/utils/api';
 import Loading from '@/app/_components/common/loading/Loading';
-import { useEffect } from 'react';
 import { getTokenFromStorage } from "@/context/AuthContext";
 
 export default function Page() {
    const accessToken = getTokenFromStorage("access_token");
    const searchParams = useSearchParams();
-   const router = useRouter();
+   // const router = useRouter();
    const sessionId = searchParams.get('session_id');
-   const lang = localStorage.getItem("lang");
+   // const lang = localStorage.getItem("lang");
    const { data, loading } = useFetch(`${API_BASE_URL}/users/me/subscription/confirm?session_id=${sessionId}`, {
       method: 'POST',
       headers: {
@@ -21,11 +20,11 @@ export default function Page() {
       }
    });
 
-   useEffect(() => {
-      if (data) {
-         router.push(`/${lang}/site/plans/purchase/confirm?session_id=${sessionId}`);
-      }
-   }, []);
+   // useEffect(() => {
+   //    if (data) {
+   //       router.push(`/${lang}/site/plans/purchase/confirm?session_id=${sessionId}`);
+   //    }
+   // }, []);
 
    if (loading) {
       return <Loading />;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,30 @@
+import { NextResponse } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+// Define supported locales
+const supportedLocales = ['en', 'ar'];
+
+export default function middleware(req: any) {
+   const { pathname } = req.nextUrl;
+
+   // Split the pathname and check if the first part matches a locale
+   const pathnameParts = pathname.split('/');
+   const localeInPath = pathnameParts[1];
+
+   // Get the locale from the "NEXT_LOCALE" cookie
+   const cookieLocale = req.cookies.get('NEXT_LOCALE')?.value || 'ar';
+
+   // If the path doesn't start with a locale, redirect to the locale from the cookie
+   if (!supportedLocales.includes(localeInPath)) {
+      const redirectUrl = new URL(`/${cookieLocale}${pathname}`, req.url);
+      return NextResponse.redirect(redirectUrl);
+   }
+
+   // If the locale is already in the path, continue with the intl middleware
+   return createMiddleware(routing)(req);
+}
 
 export const config = {
-   matcher: ['/', '/(en|ar)/:path*', '/(en|ar)/site/:path*', '/(en|ar)/dashboard/:path*']
+   matcher: ['/', '/(en|ar)/:path*', '/site/:path*', '/dashboard/:path*'],
 };
