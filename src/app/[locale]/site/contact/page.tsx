963172f7dc8b4bbe417c1639ed22c529ed4ba20c
mahmoud <mahmoudshalaby363@gmail.com>
2024-11-26 19:30:54 +0200

    test something at free trial

--- src/app/[locale]/site/contact/page.tsx
+++ src/app/[locale]/site/contact/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
                <div className="flex flex-col justify-start ">
                   <h3 className="text-2xl font-bold">{t("telgram")}</h3>
                   <p className="text-lg py-3 text-gray-800">{t("chatTelgram")}</p>
-                  <Link href="https://t.me/FinTechFusio0" target="_blank">
+                  <Link href="" target="_blank">
                      <button className="w-fit rounded-md px-4 py-2 text-dark bg-secondary text-lg tracking-wide border hover:bg-dark hover:text-secondary">{t("openTelgram")}</button>
                   </Link>
                </div>
