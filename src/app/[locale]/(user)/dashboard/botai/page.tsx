ca384ec5ba3cf0f1ac7cf63058f631ac5c5e7bb5
mahmoud <mahmoudshalaby363@gmail.com>
2024-11-30 16:26:02 +0200

    remove image at coming soon page

--- src/app/[locale]/(user)/dashboard/botai/page.tsx
+++ src/app/[locale]/(user)/dashboard/botai/page.tsx
@@ -3,8 +3,6 @@
 // import UserStrategy from "@/app/_components/common/user/UserStrategy";
 
 import Textbox from "@/app/_components/common/Text/Textbox";
-import botaiImg from "/public/assets/images/botaiImg.webp";
-import Image from "next/image";
 
 export default function page() {
   return (
@@ -17,13 +15,6 @@ export default function page() {
         <TradingBotForm type="ai" />
       </div> */}
       <Textbox title="Our Ai Bot is coming soon!" description="Get ready for an intelligent conversation experience. Our Al bot will revolutionize how you interact with our webapp." />
-      {/* <Image
-        src={botaiImg}
-        alt="bot-ai"
-        width="450"
-        height="450"
-        className="mx-auto w-auto h-auto rounded"
-      /> */}
     </div>
   )
 } 
